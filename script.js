53d37b05d80959cda67f667e1ba20c910bb6ca7e
Jim Ryan <jimmih15@gmail.com>
2020-02-08 19:14:49 +0000

    js added

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 
 
 
-function updateQuote(){
-    var quote=document.getElementsByClassName('quotes');
-    var changeText = quote[0].innerHTML="hello";
+window.onload = function() {
+    var owQuotes = ["I have the simplest tastes. I am always satisfied with the best.",
+    "One of the many lessons that one learns in prison is, that things are what they are and will be what they will be.",
+  "The only thing to do with good advice is to pass it on. It is never of any use to oneself.",
+"Man is least himself when he talks in his own person. Give him a mask, and he will tell you the truth.",
+"Experience is simply the name we give our mistakes.",
+"Always forgive your enemies - nothing annoys them so much.",
+"Selfishness is not living as one wishes to live, it is asking others to live as one wishes to live.",
+"Keep love in your heart. A life without it is like a sunless garden when the flowers are dead."
+
+]
+  
+  const btn = document.getElementById('new-quote');
+
+
+
+btn.onclick = function() {
+
+document.getElementById("text").innerHTML=owQuotes[Math.floor(Math.random() * owQuotes.length)]
 }
+}
+
